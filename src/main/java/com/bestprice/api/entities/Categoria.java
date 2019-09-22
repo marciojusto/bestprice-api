8780b4f8633190fb450f376fa20f1f69dc0082de
Marcio Justo <marcio.justo@gmail.com>
2019-09-22 17:52:02 +0100

    Algumas configuracoes de banco de dados

---
+++ src/main/java/com/bestprice/api/entities/Categoria.java
@@ -0,0 +1,9 @@
+package com.bestprice.api.entities;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = -8296583361218198012L;
+
+}
---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,11 @@
+CREATE TABLE secao (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO secao (nome) VALUES ('Higiene');
+INSERT INTO secao (nome) VALUES ('Limpeza');
+INSERT INTO secao (nome) VALUES ('Peixaria');
+INSERT INTO secao (nome) VALUES ('Charcutaria');
+INSERT INTO secao (nome) VALUES ('Bebidas');
+
