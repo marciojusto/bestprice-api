d5ef5b1d9a442a3a917f15cb1eb110ce0402226c
Marcio Justo <marciojusto@MBP-de-Marcio.lan>
2019-09-22 12:23:41 +0100

    Arquivos iniciais do projeto

---
+++ src/main/java/com/bestprice/api/BestpriceWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.bestprice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestpriceWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestpriceWebApiApplication.class, args);
+	}
+
+}
