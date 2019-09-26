e5a816880c5234bfed8f0c98d873d8595029cd00
Marcio Justo <marcio.justo@gmail.com>
2019-09-26 22:48:19 +0100

    Finalizando o modelo

--- src/main/resources/db/migration/V02__criar_tabela_mercado_e_relacionar_com_secao.sql
+++ src/main/resources/db/migration/V02__criar_tabela_mercado_e_relacionar_com_secao.sql
@@ -1,6 +1,12 @@
 CREATE TABLE mercado (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(30) NOT NULL,
+	id_mercado INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE mercado_secao (
+	id_mercado INT,
 	id_secao INT,
-	CONSTRAINT fk_secao FOREIGN KEY (id_secao) REFERENCES secao (id)
+	CONSTRAINT fk_mercado_ms_pk FOREIGN KEY (id_mercado) REFERENCES mercado (id_mercado),
+	CONSTRAINT fk_secao_ms_pk FOREIGN KEY (id_secao) REFERENCES secao (id_secao),
+	PRIMARY KEY (id_mercado, id_secao)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/main/resources/db/migration/V04__criar_tabela_preco.sql
+++ src/main/resources/db/migration/V04__criar_tabela_preco.sql
@@ -1,11 +1,23 @@
 create table preco (
-	id_produto int not null,
-	id_mercado int not null,
+	id_preco BIGINT(20) AUTO_INCREMENT,
+	id_produto BIGINT(20) NOT NULL,
+	id_mercado INT NOT NULL,
 	data DATE NOT NULL,
-	valor decimal(5,2) not null,
-	PRIMARY KEY (id_produto, id_mercado, data)
+	valor decimal(5,2) NOT NULL,
+	status TINYINT(1) NOT NULL DEFAULT 1,
+	constraint fk_produto_pk foreign key (id_produto) REFERENCES produto (id_produto),
+	constraint fk_mercado_pk foreign key (id_mercado) REFERENCES mercado (id_mercado),
+	PRIMARY KEY (id_preco, id_produto, id_mercado)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-alter table preco 
-	add constraint fk_produto_pk foreign key (id_produto) REFERENCES produto (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	add constraint fk_mercado_pk foreign key (id_mercado) REFERENCES mercado (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE produto
+	ADD COLUMN id_preco BIGINT(20);
+	
+ALTER TABLE mercado
+	ADD id_preco BIGINT(20);
+
+ALTER TABLE produto
+	ADD CONSTRAINT fk_preco_produto FOREIGN KEY (id_preco) REFERENCES preco (id_preco) ON DELETE CASCADE;
+	
+ALTER TABLE mercado
+	ADD CONSTRAINT fk_preco_mercado FOREIGN KEY (id_preco) REFERENCES preco (id_preco) ON DELETE CASCADE;
